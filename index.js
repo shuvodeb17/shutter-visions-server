9462a0516edf5588beb0379865ae5be4899b2a6e
Shuvo Deb <shuvodeb17014@gmail.com>
2023-06-12 03:25:32 +0600

    create payment intent

--- index.js
+++ index.js
@@ -196,6 +196,19 @@ async function run() {
     })
 
 
+    // const create payment intent 
+    app.post('/create-payment-intent', async (req, res) => {
+      const {price}  = req.body;
+      const amount = parseInt(price * 100);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ['card']
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    })
 
 
 
